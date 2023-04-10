5aabb815d77406ecf39ecc7d6753271505988db9
andrevigorito <34506311+andrevigorito@users.noreply.github.com>
2023-04-10 06:33:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,27 +15,27 @@ export default function Home() {
           A privacidade dos usuários é uma prioridade para a Gera Sinergia. Esta Política de Privacidade descreve como as informações pessoais dos usuários são coletadas, usadas e compartilhadas quando se utiliza o Sistema WebCOL da Gera Sinergia.
         </p>
         <p className="description">
-          <h2>Informações Coletadas</h2>
+          Informações Coletadas<br />
           O Sistema WebCOL da Gera Sinergia coleta informações pessoais dos usuários, incluindo nome, endereço de e-mail e informações de contato. Também coletamos informações sobre o uso do sistema, incluindo logs de acesso e outras informações de uso.
         </p>
         <p className="description">
-          <h2>Uso das Informações</h2>
+          Uso das Informações<br />
           As informações coletadas são usadas para fornecer acesso e suporte aos usuários do Sistema WebCOL da Gera Sinergia, bem como para melhorar a qualidade do sistema e seus serviços. Não utilizamos essas informações para fins de marketing, como enviar e-mails informativos e promocionais aos usuários.
         </p>
         <p className="description">
-          <h2>Compartilhamento de Informações</h2>
+          Compartilhamento de Informações<br />
           A Gera Sinergia não compartilha informações pessoais dos usuários com terceiros.
         </p>
         <p className="description">
-          <h2>Segurança das Informações</h2>
+          Segurança das Informações<br />
           A Gera Sinergia toma medidas para garantir que as informações pessoais dos usuários sejam protegidas contra acesso não autorizado ou divulgação. Isso inclui o uso de criptografia de dados e outras medidas de segurança para proteger as informações durante a transmissão e armazenamento.
         </p>
         <p className="description">
-          <h2>Alterações nesta Política de Privacidade</h2>
+          Alterações nesta Política de Privacidade<br />
           Podemos atualizar esta Política de Privacidade periodicamente para refletir mudanças em nossas práticas de privacidade. Notificaremos os usuários de quaisquer mudanças significativas na política e obteremos o consentimento do usuário, quando exigido por lei.
         </p>
         <p className="description">
-          <h2>Contato</h2>
+          Contato<br />
           Se os usuários tiverem dúvidas ou preocupações sobre esta Política de Privacidade ou sobre o tratamento de suas informações pessoais pelo Sistema WebCOL da Gera Sinergia, devem entrar em contato conosco através do e-mail support@gerasinergia.zendesk.com.
         </p>
         <p className="description">
